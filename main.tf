c5c71ad38ab1009a15974c4df25aecae23c0bc1e
Tu Nguyen <im2nguyen@users.noreply.github.com>
2020-02-20 15:39:19 -0800

    add main.tf for katacoda

---
+++ main.tf
@@ -0,0 +1,95 @@
+provider "consul" {
+  address    = "[[HOST_SUBDOMAIN]]-8500-[[KATACODA_HOST]].environments.katacoda.com"
+  datacenter = "dc1"
+  token      = "<ACL_TOKEN_HERE>"
+}
+
+# Register Consul Node - counting
+resource "consul_node" "counting" {
+  name    = "counting"
+  address = "[[HOST_SUBDOMAIN]]-9001-[[KATACODA_HOST]].environments.katacoda.com"
+}
+
+# Register Consul Node - dashboard
+resource "consul_node" "dashboard" {
+  name    = "dashboard"
+  address = "[[HOST_SUBDOMAIN]]-8080-[[KATACODA_HOST]].environments.katacoda.com"
+}
+
+# Register Counting Service
+resource "consul_service" "counting" {
+  name    = "counting-service"
+  node    = consul_node.counting.name
+  port    = 80
+  tags    = ["counting"]
+
+  check {
+    check_id                          = "service:counting"
+    name                              = "Counting health check"
+    status                            = "passing"
+    http                              = "[[HOST_SUBDOMAIN]]-9001-[[KATACODA_HOST]].environments.katacoda.com"
+    tls_skip_verify                   = false
+    method                            = "GET"
+    interval                          = "5s"
+    timeout                           = "1s"
+    deregister_critical_service_after = "30s"
+
+    header {
+      name  = "foo"
+      value = ["bar"]
+    }
+  }
+}
+
+# Register Dashboard Service
+resource "consul_service" "dashboard" {
+  name    = "dashboard-service"
+  node    = consul_node.dashboard.name
+  port    = 80
+  tags    = ["dashboard"]
+
+  check {
+    check_id                          = "service:dashboard"
+    name                              = "Dashboard health check"
+    status                            = "passing"
+    http                              = "[[HOST_SUBDOMAIN]]-8080-[[KATACODA_HOST]].environments.katacoda.com"
+    tls_skip_verify                   = false
+    method                            = "GET"
+    interval                          = "5s"
+    timeout                           = "1s"
+    deregister_critical_service_after = "30s"
+
+    header {
+      name  = "foo"
+      value = ["bar"]
+    }
+  }
+}
+
+# List all services
+data "consul_services" "dc1" {}
+
+output "consul_services_dc1" {
+    value = data.consul_services.dc1
+}
+
+# List counting service information
+data "consul_service" "counting" {
+  name = consul_service.counting.name
+}
+
+output "counting_ports" {
+  value = data.consul_service.counting
+}
+
+# List Consul agent node address and ports
+data "consul_service" "agents" {
+  name = "consul"
+}
+
+output "consul_agents_address_ports" {
+  value = {
+    for service in data.consul_service.agents.service:
+    service.node_id => join(":", [service.node_address, service.port])
+  }
+}
